e33a234647dda8a588fd830c36e98893c30aad2e
Georgina Garza <31809002+gigarza@users.noreply.github.com>
2020-08-28 00:45:37 -0500

    More DB Updates

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ else:
         app.config[key] = val
 
 # Get the database
-client = MongoClient('mongodb+srv://'+app.config['mongo_user']+':'+app.config['mongo_pass']+'@'+app.config['mongo_host']+'?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://'+app.config['mongo_user']+':'+app.config['mongo_pass']+'@'+app.config['mongo_host'])
 db = client.get_database()
 
 
